dbc29eff37acb303829e4691c5981246af428c1e
Dzybystim <103567184+Dzybystim@users.noreply.github.com>
2022-09-10 13:55:42 +0300

    27

--- 26.js
+++ 26.js
@@ -1,63 +1,24 @@
-"use strict"
+const fetchUserFromServer = (username, onSuccess, onError) => {
+  console.log(`Fetching data for ${username}`);
 
-// Напишіть функцію addItemToBasket, яка буде за допомогою методу find перевіряти, чи є вже такий продукт в кошику
-// Якщо він є, то потроібно оновити його в коризні
-// Якщо немає, то потрібно додати новий продукт до кошику покупок
+  setTimeout(() => {
+    // Change value of isSuccess variable to simulate request status
+    const isSuccess = true;
 
-// Після цього порахувати суму всіх покупок і вивести total;
+    if (isSuccess) {
+      onSuccess("success value");
+    } else {
+      onError("error");
+    }
+  }, 2000);
+};
 
- let total = 0;
+const onFetchSuccess = user => {
+  console.log(user);
+};
 
- const product1 = {
-  id: 33,
-  title: 'T-shirt',
-  price: 99,
-  count: 1,
-}
+const onFetchError = error => {
+  console.error(error);
+};
 
-const product2 = {
-  id: 29,
-  title: 'Jeans',
-  price: 199,
-  count: 1,
-}
-
-
-const basketItems = [
-  {
-    id: 28,
-    title: 'iPhone 14',
-    price: 1599,
-    count: 3,
-  },
-  {
-    id: 29,
-    title: 'Jeans',
-    price: 199,
-    count: 2,
-  }
-];
-
-
- function addItemToBasket (product) {
-const productInBascket = basketItems.find( item => item.id === product.id)
-if (productInBascket) {
-  return basketItems.map(item => {
-    if (item.id===product.id) {
-      return {
-        ...item, 
-        count: product.count + item.count
-      }
-    } else return item
-    
-  } )
-} else {
-  return [...basketItems, product]
-}
-
-console.log(productInBascket)
-}
-
-
-console.log(addItemToBasket(product1))
-console.log(addItemToBasket(product2))
+fetchUserFromServer("Mango", onFetchSuccess, onFetchError);
